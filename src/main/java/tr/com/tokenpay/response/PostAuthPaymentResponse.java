b6d28ee8a00a80701e1e16a42eea93555ef68bd3
oguzhan.ince <oguzhan.ince@tokenpay.com.tr>
2024-03-20 14:34:48 +0300

    dd Data annot.

--- src/main/java/tr/com/tokenpay/response/PostAuthPaymentResponse.java
+++ src/main/java/tr/com/tokenpay/response/PostAuthPaymentResponse.java
@@ -1,9 +1,11 @@
 package tr.com.tokenpay.response;
 
+import lombok.Data;
 import tr.com.tokenpay.response.dto.PaymentMultiCardStatus;
 
 import java.util.List;
 
+@Data
 public class PostAuthPaymentResponse {
 
     private String conversationId;
